36ecf0a201ef19d3e793f39c2553e667e9a2f7c5
DAndyPhm0609 <101655423+DAndyPhm0609@users.noreply.github.com>
2022-07-26 14:22:10 +0700

    Add files via upload

---
+++ NavigationViews.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationViews.swift
+//  Assign1_s3818286
+//
+//  Created by An Pham Hoang Thien on 26/07/2022.
+//
+
+import SwiftUI
+
+struct NavigationViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationViews_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationViews()
+    }
+}
